d50f120d9dfc6f0a4e73c93c909665c53dc7f896
PeryCreep <polyakov02.artem@bk.ru>
2024-05-06 17:04:20 +0500

    Большой рефакторинг, но все равно не достаточный)

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,38 +1,81 @@
+import Main.getAllMessageInGame
 import cats.effect.{ExitCode, IO, IOApp}
+import cats.implicits._
+import configuration.ServerConf
+import domain.messages.MessageCompanionObject.MessageJsonOps
 import domain.messages.{Message, MessageCompanionObject}
-import domain.{GameCompanionObject, db}
+import domain.{Game, GameRepository, UserDTO, db}
 import fs2.concurrent.Topic
 import fs2.{Pipe, Stream}
 import io.circe.generic.auto.exportEncoder
 import io.circe.parser
-import org.http4s.HttpRoutes
-import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
+import io.circe.parser._
+import org.http4s.circe.CirceEntityCodec._
 import org.http4s.dsl.io._
 import org.http4s.implicits._
-import cats.implicits.toSemigroupKOps
-import domain.messages.MessageCompanionObject.MessageJsonOps
 import org.http4s.server.websocket.WebSocketBuilder2
 import org.http4s.websocket.WebSocketFrame
+import org.http4s.{HttpRoutes, Request, Response, headers}
+import org.typelevel.ci.CIString
+import pdi.jwt.{JwtAlgorithm, JwtCirce}
 
 object Main extends IOApp {
 
   private val topic = Topic[IO, Message]
-  private def wsRoute(ws: WebSocketBuilder2[IO], topic: Topic[IO, Message]): HttpRoutes[IO] = HttpRoutes.of[IO] {
-    case GET -> Root / "ws" / LongVar(gameId)  =>//todo check that user can write in this game
-      /** create subscribe stream for client */
-      val send: Stream[IO, WebSocketFrame] = topic.subscribe(100)
-        .filter(_.gameId == gameId)
-        .map { msg =>
-          WebSocketFrame.Text(msg.toJson.toString())
-        }
+  private val serverConf = ServerConf.instance
 
-      /** create pipe for input stream*/
-      val receive: Pipe[IO, WebSocketFrame, Unit] = stream => stream.collect {
+  private val jwtKey = serverConf.jwtKey
+
+  private def wsRoute(ws: WebSocketBuilder2[IO], topic: Topic[IO, Message]): HttpRoutes[IO] = HttpRoutes.of[IO] {
+    case  req @ GET -> Root / "ws" / LongVar(gameId) =>
+      for {
+        token <- extractTokenFromHeader(req)
+        userOpt <- IO.pure(decryptToken(token))
+        gameOpt <- getGameById(gameId)
+        userInGameOpt <- IO.pure(checkUserInGame(gameOpt, userOpt))
+        response <- userInGameOpt.map { userInGame =>
+          if(userInGame) buildWebSocket(gameId, ws, topic) else Forbidden("Пользователя нет в данной игре!")
+        }.getOrElse(NotFound("Такой игры не существует или в токене не хранится необходимая информация"))
+      } yield response
+  }
+
+  private def checkUserInGame(gameOpt: Option[Game], userOpt: Option[UserDTO]): Option[Boolean] = {
+    (gameOpt, userOpt).tupled.map { case (game, user) =>
+      game.participants.contains(user.id)
+    }
+  }
+
+  private def extractTokenFromHeader(req: Request[IO]): IO[String] = {
+    headers.Authorization
+    req.headers.get(CIString("token"))
+      .map(_.head.value)
+      .fold(IO.raiseError[String](new Exception("Токен не найден в заголовке запроса!")))(x => IO.pure(x))
+  }
+
+  private def decryptToken(token: String): Option[UserDTO] = {
+
+    for {
+      claim <- JwtCirce.decode(token, jwtKey, Seq(JwtAlgorithm.HS256)).toOption
+      userDataJson <- parse(claim.content).toOption
+      userData <- userDataJson.as[UserDTO].toOption
+    } yield userData
+  }
+
+  private def buildWebSocket(gameId: Long, ws: WebSocketBuilder2[IO], topic: Topic[IO, Message]): IO[Response[IO]] = {
+    /** create subscribe stream for client */
+    val send: Stream[IO, WebSocketFrame] = topic.subscribe(100)
+      .filter(_.gameId == gameId)
+      .map { msg =>
+        WebSocketFrame.Text(msg.toJson.toString())
+      }
+
+    /** create pipe for input stream */
+    val receive: Pipe[IO, WebSocketFrame, Unit] = stream => stream.collect {
         case WebSocketFrame.Text(text, _) =>
           parser.parse(text)
-          .map(MessageCompanionObject.messageDTOJsonDecoder.decodeJson)
+            .map(MessageCompanionObject.messageDTOJsonDecoder.decodeJson)
       }
-      .evalMap{
+      .evalMap {
         case Right(Right(messageDTO)) =>
           IO.pure {
             val message = messageDTO.toMessage(gameId)
@@ -43,39 +86,61 @@ object Main extends IOApp {
       }
       .through(topic.publish)
 
-
-      /** check that game exists */
-      IO.fromFuture(IO.pure(db.run(GameCompanionObject.checkGameExists(gameId)))).flatMap {
-        case true => ws.build(send, receive)
-        case false => NotFound("Такой игры не существует")
-      }
+    ws.build(send, receive)
   }
+
+  private def getGameById(gameId: Long): IO[Option[Game]] =
+    IO.fromFuture(IO.pure(db.run(GameRepository.getGameById(gameId))))
+
+  private def getAllMessageInGame(gameId: Long): IO[Seq[Message]] = {
+    IO.fromFuture(IO.pure(db.run(MessageCompanionObject.getMessagesByGameId(gameId))))
+  }
+
 
   override def run(args: List[String]): IO[ExitCode] = {
     /** routes for rest */
-    val routes = HttpRoutes.of[IO] {
-      case POST -> Root / "room" / "create"/ LongVar(userId) =>
-        IO.fromFuture { IO {
-          db.run(GameCompanionObject.createGame(userId))
-        }}.flatMap { gameId =>
+    val routes = HttpRoutes.of[IO] { //todo добавить мониторинг нагрузки
+      case req @ POST -> Root / "game" / "create" =>
+        IO.fromFuture {
+          for {
+            userId <- req.as[Long]
+          } yield {
+            db.run(GameRepository.createGame(userId))
+          }
+        }.flatMap { gameId =>
           Ok(gameId.toString)
         }
 
-      case GET -> Root / "room" / LongVar(gameId) => //todo check that user can write in this game
-        IO.fromFuture(IO.pure(db.run(MessageCompanionObject.getMessagesByGameId(gameId)))).flatMap { messages =>
-          Ok(messages)
-        }
+      case req @ GET -> Root / "game" / LongVar(gameId) =>
+        for {
+          token <- extractTokenFromHeader(req)
+          userOpt <- IO.pure(decryptToken(token))
+          gameOpt <- getGameById(gameId)
+          userInGameOpt <- IO.pure(checkUserInGame(gameOpt, userOpt))
+          response <- userInGameOpt.map { userInGame =>
+            if(userInGame)
+              Ok(getAllMessageInGame(gameId))
+            else Forbidden("Пользователя нет в данной игре!")
+          }.getOrElse(NotFound("Такой игры не существует или в токене не хранится необходимая информация"))
+        } yield response
 
+      case req @ POST -> Root / "game" / LongVar(gameId) / "participants" =>
+        for {
+          participantId <- req.as[Long]
+          result <- IO.fromFuture(IO.pure(db.run(GameRepository.addParticipant(gameId, participantId))))
+          response <- if (result > 0) Ok("Participant added to the game")
+          else NotFound("Game not found")
+        } yield response
     }
 
     def routesBuilder(topic: Topic[IO, Message]) = org.http4s.blaze.server.BlazeServerBuilder[IO]
-      .bindHttp(9081, "0.0.0.0")//todo conf
+      .bindHttp(serverConf.port, serverConf.host)
       .withHttpWebSocketApp(ws => (wsRoute(ws, topic) <+> routes).orNotFound)
 
     val program = for {
       _ <- IO(println("Initializing database"))
-      _ <- IO.fromFuture(IO(db.run(MessageCompanionObject.createTableIfNotExists)))//todo вынести в отдельный класс
-      _ <- IO.fromFuture(IO(db.run(GameCompanionObject.createTableIfNotExists)))
+      _ <- IO.fromFuture(IO(db.run(MessageCompanionObject.createTableIfNotExists))) //todo вынести в отдельный класс
+      _ <- IO.fromFuture(IO(db.run(GameRepository.createTableIfNotExists)))
       t <- topic //todo create queue because topic wait that all messages will be delivered
       // see https://habr.com/ru/articles/517076/
       _ <- IO.println("Server Started ")
--- src/main/scala/domain/Game.scala
+++ src/main/scala/domain/Game.scala
@@ -1,9 +1,36 @@
 package domain
+
+import domain.GameRepository.seqStringColumnType
 import slick.jdbc.PostgresProfile.api._
 import slick.lifted.ProvenShape
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
 case class Game(id: Option[Long], participants: Seq[Long])
 
-class GameTable(tag: Tag) extends Table[Game](tag, "games") {
+class GameTable(tag: Tag) extends Table[Game](tag, "games") { //todo вынестри в отдельный файл
+
+  def id: Rep[Long] = column[Long]("id", O.AutoInc, O.PrimaryKey)
+
+  def participants: Rep[Seq[Long]] = column[Seq[Long]]("participants")
+
+  override def * : ProvenShape[Game] = (id.?, participants) <> (Game.tupled, Game.unapply)
+
+}
+
+object GameRepository {
+
+  val games = TableQuery[GameTable]
+
+  def createTableIfNotExists: DBIO[Unit] =
+    games.schema.createIfNotExists
+
+  def getGameById(gameId: Long): DBIO[Option[Game]] =
+    games.filter(_.id === gameId).result.headOption
+
+  def createGame(userId: Long): DBIO[Long] =
+    games returning games.map(_.id) += Game(None, Seq(userId))
+
   // MappedColumnType для преобразования Seq в JSON и обратно
   implicit val seqStringColumnType: BaseColumnType[Seq[Long]] =
     MappedColumnType.base[Seq[Long], String](
@@ -11,23 +38,16 @@ class GameTable(tag: Tag) extends Table[Game](tag, "games") {
       str => str.split(",").map(x => x.toLong).toSeq
     )
 
-  def id: Rep[Long] = column[Long]("id", O.AutoInc, O.PrimaryKey)
+  def addParticipant(gameId: Long, participantId: Long): DBIO[Int] = {
+    import slick.jdbc.PostgresProfile.api._
+    val query = for {
+      game <- games if game.id === gameId
+    } yield game
 
-  def participants: Rep[Seq[Long]] = column[Seq[Long]]("participants")
-  override def * : ProvenShape[Game] = (id.?, participants) <> (Game.tupled, Game.unapply)
-}
-object GameCompanionObject {
-
-  val games = TableQuery[GameTable]
-  def createTableIfNotExists: DBIO[Unit] =
-    games.schema.createIfNotExists
-
-  def checkGameExists(gameId: Long): DBIO[Boolean] =
-    games.filter(_.id === gameId).exists.result
-
-  def createGame(userId: Long): DBIO[Long] =
-    games returning games.map(_.id) += Game(None,Seq(userId))
-
-  def getGames: DBIO[Seq[Game]] =
-    games.result
+    query.result.head.flatMap { game =>
+      val updatedParticipants = game.participants :+ participantId
+      val updateQuery = games.filter(_.id === gameId).map(_.participants).update(updatedParticipants)
+      updateQuery
+    }.transactionally
+  }
 }
